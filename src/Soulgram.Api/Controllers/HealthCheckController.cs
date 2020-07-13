b725fc94cba1fb0faeaf8a0633b79af182c89dc3
Дима Киреев <dimanoit@gmail.com>
2020-07-13 22:27:35 +0300

    add: serilog console logging

--- src/Soulgram.Api/Controllers/HealthCheckController.cs
+++ src/Soulgram.Api/Controllers/HealthCheckController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Soulgram.Api
 {
     [Route("/hc")]
     public class HealthCheckController : ControllerBase
     {
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
+            _logger.LogInformation("Health check");
             return Ok();
         }
 
--- src/Soulgram.Api/Program.cs
+++ src/Soulgram.Api/Program.cs
@@ -1,8 +1,8 @@
-using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Soulgram.Common;
+using System;
 
 namespace Soulgram.Api
 {
@@ -10,15 +10,39 @@ namespace Soulgram.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            ConfigureLogging();
+
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .SetupConfiguration()
+                .UseSerilog() 
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void ConfigureLogging()
+        {
+            //TODO add more logging options via appsettings, configure log levels, write logs to files 
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
     }
 }
